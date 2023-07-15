40b6a5c067912af6862faad07c09bb399d3f7b1a
Christopher Ortiz <53792120+cortiz0550@users.noreply.github.com>
2023-07-14 20:20:05 -0700

    Update api_calls.py
    Update with classes

--- qx_downloads/api_calls.py
+++ qx_downloads/api_calls.py
@@ -1,57 +1,70 @@
 import requests
 import zipfile
 import json
-import io, os
+import io
 
-def exportSurvey(apiToken, surveyId, dataCenter, fileFormat):
-    surveyId = surveyId
-    fileFormat = fileFormat
-    dataCenter = dataCenter 
 
-    # Setting static parameters
-    requestCheckProgress = 0.0
-    progressStatus = "inProgress"
-    baseUrl = "https://{0}.qualtrics.com/API/v3/surveys/{1}/export-responses/".format(dataCenter, surveyId)
-    headers = {
-    "content-type": "application/json",
-    "x-api-token": apiToken,
-    }
+class SurveyDownload:
+
+    def __init__(self, survey_id, data_center, api_token, split_cols=True, numeric_recode=False, file_format='csv'):
+        self.survey_id = survey_id
+        self.file_format = file_format
+        self.split_cols = split_cols
+        self.numeric_recode = numeric_recode
+
+        # def variables
+        self.base_url = f"https://{data_center}.qualtrics.com/API/v3/surveys/{survey_id}/export-responses/"
+        self.headers = {
+        "content-type": "application/json",
+        "x-api-token": api_token
+        }
+        self.progress_status = 'inProgress'
+
+
+    def __repr__(self):
+        return f'Survey ID: {self.survey_id}'
+
+
+    def create_response(self):
+        download_request_url = self.base_url
+        download_request_payload = '{"format":"' + self.file_format + '"}'
+        download_request_response = requests.request("POST", download_request_url, data=download_request_payload, headers=self.headers)
+        print(download_request_response.json())
+        progress_id = download_request_response.json()["result"]["progressId"]
+        #print(download_request_response.text)
+
+        if progress_id is None or progress_id == "":
+            return('progressId is empty')
+        else:
+            return progress_id
+
+
+    def get_response_progress(self, progress_id):
+        while self.progress_status != "complete" and self.progress_status != "failed":
+            #print ("progress_status=", progress_status)
+            request_check_url = self.base_url + progress_id
+            request_check_response = requests.request("GET", request_check_url, headers=self.headers)
+            request_check_progress = request_check_response.json()["result"]["percentComplete"]
+            #print("Download is " + str(request_check_progress) + " complete")
+            self.progress_status = request_check_response.json()["result"]["status"]
     
+        #step 2.1: Check for error
+        if self.progress_status == "failed":
+            raise Exception("export failed")
 
-    # Step 1: Creating Data Export
-    downloadRequestUrl = baseUrl
-    downloadRequestPayload = '{"format":"' + fileFormat + '"}'
-    downloadRequestResponse = requests.request("POST", downloadRequestUrl, data=downloadRequestPayload, headers=headers)
-    #print(downloadRequestResponse.json())
-    progressId = downloadRequestResponse.json()["result"]["progressId"]
-    #print(downloadRequestResponse.text)
-    
-    # This piece stops the process if we have a 200 response code, but no progress ID
-    if progressId is None or progressId == "":
-        raise Exception("progressId is empty")
+        file_id = request_check_response.json()["result"]["fileId"]
+        #print(file_id)
+        return file_id
 
-    
-    # Step 2: Checking on Data Export Progress and waiting until export is ready
-    while progressStatus != "complete" and progressStatus != "failed":
-        #print ("progressStatus=", progressStatus)
-        requestCheckUrl = baseUrl + progressId
-        requestCheckResponse = requests.request("GET", requestCheckUrl, headers=headers)
-        requestCheckProgress = requestCheckResponse.json()["result"]["percentComplete"]
-        #print("Download is " + str(requestCheckProgress) + " complete")
-        progressStatus = requestCheckResponse.json()["result"]["status"]
-    
-    #step 2.1: Check for error
-    if progressStatus == "failed":
-        raise Exception("export failed")
 
-    fileId = requestCheckResponse.json()["result"]["fileId"]
-    
+    def get_response(self, file_id):
+        request_download_url = self.base_url + file_id + '/file'
+        request_download = requests.request("GET", request_download_url, headers=self.headers, stream=True)
+        zipfile.ZipFile(io.BytesIO(request_download.content)).extractall()
+        print('Complete')
 
-    # Step 3: Downloading file
-    requestDownloadUrl = baseUrl + fileId + '/file'
-    requestDownload = requests.request("GET", requestDownloadUrl, headers=headers, stream=True)
 
-    
-    # Step 4: Unzipping the file
-    zipfile.ZipFile(io.BytesIO(requestDownload.content)).extractall()
-    print('Complete')
+    def export_survey_response(self):
+        progress = self.create_response()
+        file = self.get_response_progress(progress)
+        self.get_response(file)
